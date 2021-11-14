eb87a2390794cb2e532e50259a309c127063c8b7
ganglyimp <titiski15@gmail.com>
2021-11-13 20:05:52 -0500

    Applied labels to waldo dataset and set up batch loader.

--- waldo.py
+++ waldo.py
@@ -1,3 +1,6 @@
+# ================
+#  PREPROCESSING
+# ================
 import cv2 
 import os
 import glob
@@ -15,6 +18,11 @@ def loadDirectory(filepath, isOrig):
     arr = []
     for fl in files:
         img = cv2.imread(fl)
+
+        # resize image to 256x256
+        if(isOrig != True and img.shape[0] != 256): 
+            img = cv2.resize(img, (256, 256))
+
         arr.append(torch.Tensor(img))
 
     # if dataset is original puzzles, return a list of tensors (images are not equal sizes)
@@ -39,3 +47,55 @@ notWaldo128 = loadDirectory("./128/notwaldo/*.jpg", False)
 
 waldo256 = loadDirectory("./256/waldo/*.jpg", False)
 notWaldo256 = loadDirectory("./256/notwaldo/*.jpg", False)
+
+# Combining into two lists: waldos and not waldos
+waldos = torch.cat((waldo64, waldo128, waldo256), 0)
+notWaldos = torch.cat((notWaldo64, notWaldo128, notWaldo256), 0)
+
+# Issue: 97 Waldo : 6940 Not Waldo
+# Need to augment dataset to inflate Waldo instances
+
+# Create labels & combine
+waldoLabels = torch.cat((torch.ones(len(waldos)), torch.zeros(len(notWaldos))), 0)
+allWaldos = torch.cat((waldos, notWaldos), 0)
+
+waldoDataset = torch.utils.data.TensorDataset(allWaldos, waldoLabels)
+
+
+# ============
+#   THE CNN
+# ============
+import torch.nn as nn
+import torch.nn.functional as F 
+import torchvision
+import torchvision.transforms as tf 
+import torch.optim as optim
+
+# Network should output whether or not the input image has waldo in it
+class WaldoFinder(nn.Module):
+    def __init__(self):
+        super(WaldoFinder, self).__init__()
+
+        C = 256 #in/out channels
+        K = 3 #kernel size
+
+        # Convolutional Layers
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=C, kernel_size=K, stride=2, padding=1)
+
+        # Scales weights by gain parameter
+        nn.init.xavier_uniform_(self.conv1.weight)
+    
+    def forward(self, t):
+        t = self.conv1(t)
+        t = F.relu(t)
+
+        return t
+
+waldoFinder = WaldoFinder()
+
+# Divide into training and test set
+trainLoader = torch.utils.data.DataLoader(waldoDataset, shuffle=True, batch_size=10)
+items, labels = next(iter(trainLoader))
+
+optimizer = optim.Adam(waldoFinder.parameters(), lr=.01)
+lossFunc = nn.MSELoss()
